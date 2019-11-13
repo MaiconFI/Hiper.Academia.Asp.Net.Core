b1bc6cd28e8310aaeec3f185aed8ceae6c63eacc
Jean Bairro <jeancasulo@gmail.com>
2019-11-13 20:29:23 -0300

    Criando middlewares de verificação de horário.

---
+++ Hiper.Academia.Asp.Net.Core.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Hiper.Academia.Asp.Net.Core.Web.Middlewares.TimeCheck;
+using Microsoft.AspNetCore.Builder;
+
+namespace Hiper.Academia.Asp.Net.Core.Web.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseTimeCheck(this IApplicationBuilder builder)
+            => builder.UseMiddleware<TimeCheckMiddleware>();
+    }
+}
---
+++ Hiper.Academia.Asp.Net.Core.Web/Middlewares/TimeCheck/TimeCheckMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Hiper.Academia.Asp.Net.Core.Web.Middlewares.TimeCheck
+{
+    public class TimeCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TimeCheckMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (AplicacaoDisponivelParaTransacoes) 
+                await _next(context);
+            else
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync("Você só pode realizar trasações até às 21:59:59");
+            }
+        }
+
+        private bool AplicacaoDisponivelParaTransacoes => DateTime.Now.Hour > 21;
+    }
+}

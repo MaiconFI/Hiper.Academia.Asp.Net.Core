cd1029124bdd9320b2e84102e11ae3df8465b21e
Jean Bairro <jeancasulo@gmail.com>
2019-11-15 12:03:26 -0300

    Iniciando os trabalhos de front

---
+++ Hiper.Academia.AspNetCore.Web/Controllers/ContaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hiper.Academia.AspNetCore.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Sacar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Extrato()
+        {
+            return View();
+        }
+    }
+}
